51880fbdac2f777ec1c705cac727e0fa88d3d2b9
Miroslav Rovis <miro.rovis@croatiafidelis.hr>
2017-02-15 16:02:04 +0100

    If secret key available, option not go online.

--- uncenz-ipt-conf-states.sh
+++ uncenz-ipt-conf-states.sh
@@ -12,19 +12,21 @@
 
 if [ -e ".uncenz-ts" ]; then
 	tstamp_hst=$(cat .uncenz-ts)_$(hostname)
+	hst=$(hostname)
 else
 	tstamp_hst=$(date +%y%m%d_%H%M)_$(hostname)
+	hst=$(hostname)
 fi
 echo \$tstamp_hst: $tstamp_hst
-iptables -t filter -L -n -v > ipt-t_flt-L-n-v_${tstamp_hst}
-iptables -t nat -L -n -v > ipt-t_nat-L-n-v_${tstamp_hst}
-iptables -t mangle -L -n -v > ipt-t_mgl-L-n-v_${tstamp_hst}
-iptables -t raw -L -n -v > ipt-t_raw-L-n-v_${tstamp_hst}
-ip link show > ip_link_show_${tstamp_hst}
-ip addr show > ip_addr_show_${tstamp_hst}
-ip rout show > ip_rout_show_${tstamp_hst}
+iptables -t filter -L -n -v > ipt-t_flt-L-n-v_${hst} 
+iptables -t nat -L -n -v > ipt-t_nat-L-n-v_${hst} 
+iptables -t mangle -L -n -v > ipt-t_mgl-L-n-v_${hst} 
+iptables -t raw -L -n -v > ipt-t_raw-L-n-v_${hst} 
+ip link show > ip_link_show_${hst}
+ip addr show > ip_addr_show_${hst}
+ip rout show > ip_rout_show_${hst}
 #cat /etc/resolv.conf
-cp -a /etc/resolv.conf resolv_conf_${tstamp_hst}
+cp -a /etc/resolv.conf resolv_conf_${hst}
 mkdir ipt_${tstamp_hst}.d/
-mv *_${tstamp_hst} ipt_${tstamp_hst}.d/
+mv -iv ipt*-L-n-v_${hst} ip_{link,addr,rout}*_${hst} resolv_conf_${hst} ipt_conf_states_${tstamp_hst}.d/
 
